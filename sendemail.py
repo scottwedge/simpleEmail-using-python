dc364fb54cb0cf6d5b143f9e402bc3b266ce5ed3
swedge <scott.wedge@gmail.com>
2020-04-21 11:25:50 -0400

    Fixes #1

--- sendemail.py
+++ sendemail.py
@@ -8,7 +8,7 @@ def sendEmail(From, to, subject, message, mailservice, password):
     email['subject'] = subject
     email.set_content(message) 
     mail = 'smtp.' + mailservice
-    with smtplib.SMTP(host=mail, port=587) as smtp: #host could be any email service and port = 587 is the standart for the smtp protocol
+    with smtplib.SMTP(host=mail, port=587) as smtp: #host could be any email service and port = 587 is the standard for the smtp protocol
         smtp.ehlo() #this is part of the smpt standard, says hi to server essentially
         smtp.starttls() #ttls is the encrytion part, encrypts message
         smtp.login(From, password)#login of your email
@@ -18,10 +18,10 @@ def sendEmail(From, to, subject, message, mailservice, password):
 def main():
     print("Hello! Please follow the instructions to send an email using Python!\n")
     From = input("Please enter your email address: ")
-    to = input("Please enter the email of the person who will recieve the email: ")
+    to = input("Please enter the email of the person who will receive the email: ")
     subject = input("Please enter subject of the email: ")
     message = input("Please enter the main message: ")
-    mailservice = input("Please enter which email service you are using \n(ex: if you are using gmail do: gmail.com)\n ")
+    mailservice = input("Please enter which email service you are using \n(ex: if you are using gmail use: gmail.com)\n ")
     password = input("Please enter the password of your email: ")
 
     sendEmail(From, to, subject, message, mailservice, password)
