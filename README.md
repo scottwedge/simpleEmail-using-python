001df62521024024c1b807944407d7aa12bc69cd
Conner <connerccook@csu.fullerton.edu>
2020-04-20 20:40:46 -0700

    program works

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # simpleEmail-using-python
 This is a simple python program that allows you to create an email to send to one person.
+
+Warning: This program has no checks, so if you input the wrong email or mistype you will get errors. I will ger around to putting checks later on.
---
+++ sendemail.py
@@ -0,0 +1,31 @@
+import smtplib 
+from email.message import EmailMessage
+
+def sendEmail(From, to, subject, message, mailservice, password):
+    email = EmailMessage() 
+    email['from'] = From
+    email['to'] = to 
+    email['subject'] = subject
+    email.set_content(message) 
+    mail = 'smtp.' + mailservice
+    with smtplib.SMTP(host=mail, port=587) as smtp: #host could be any email service and port = 587 is the standart for the smtp protocol
+        smtp.ehlo() #this is part of the smpt standard, says hi to server essentially
+        smtp.starttls() #ttls is the encrytion part, encrypts message
+        smtp.login(From, password)#login of your email
+        smtp.send_message(email) #email is the object we used created
+        print('email sent!')
+
+def main():
+    print("Hello! Please follow the instructions to send an email using Python!\n")
+    From = input("Please enter your email address: ")
+    to = input("Please enter the email of the person who will recieve the email: ")
+    subject = input("Please enter subject of the email: ")
+    message = input("Please enter the main message: ")
+    mailservice = input("Please enter which email service you are using \n(ex: if you are using gmail do: gmail.com)\n ")
+    password = input("Please enter the password of your email: ")
+
+    sendEmail(From, to, subject, message, mailservice, password)
+  
+
+if __name__ == "__main__":
+    main()
